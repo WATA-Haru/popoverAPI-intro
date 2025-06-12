697b7e7a2f12ebcac9ea90b23aceade0cbcf7bb4
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-06-12 15:12:58 +0900

    test: ツールチップのテスト作成

---
+++ src/components/StepTooltip/index.browser.test.ts
@@ -0,0 +1,55 @@
+import { describe, test, expect } from "vitest"
+import { mount } from "@vue/test-utils"
+import StepTooltip from "./index.vue"
+
+describe("Event", () => {
+  test("初期状態ではtooltipが表示されないこと", async () => {
+    const wrapper = mount(StepTooltip, {
+      attachTo: document.body,
+      props: {
+        index: 0,
+        stepTitle: "testTitle",
+        tooltipMessage: "testMessage"
+      }
+    })
+
+    // Popoverが開いていることを確認
+    expect(wrapper.find('[data-test="tooltip"]').element.matches(":popover-open")).toBe(false)
+  })
+  test("stepをマウスホバー時にtooltipが表示されること", async () => {
+    const wrapper = mount(StepTooltip, {
+      attachTo: document.body,
+      props: {
+        index: 0,
+        stepTitle: "testTitle",
+        tooltipMessage: "testMessage"
+      }
+    })
+
+    const step = wrapper.get('[data-test="step"]');
+    const tooltip = wrapper.get('[data-test="tooltip"]');
+    await step.trigger("mouseover")
+
+    // Popoverが開いていることを確認
+    expect(tooltip.element.matches(":popover-open")).toBe(true)
+  })
+  test("マウスホバーの状態からマウスホバーが外れた場合、tooltipが表示されないこと", async () => {
+    const wrapper = mount(StepTooltip, {
+      attachTo: document.body,
+      props: {
+        index: 0,
+        stepTitle: "testTitle",
+        tooltipMessage: "testMessage"
+      }
+    })
+
+    const step = wrapper.get('[data-test="step"]');
+    const tooltip = wrapper.get('[data-test="tooltip"]');
+
+    await step.trigger("mouseover")
+    await step.trigger("mouseout")
+
+    // Popoverが閉じていることを確認
+    expect(tooltip.element.matches(":popover-open")).toBe(false)
+  })
+})
