53cb1c1241b4e6a03aff7255b70f833bf839e242
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-06-12 15:10:41 +0900

    feat: create vue vitest vitestbrowser env

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import pluginVitest from '@vitest/eslint-plugin'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  {
+    name: 'allow-not-multi-word-file',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      // ディレクトリベースのため、index.{ts,mts,tsx,vue}を使用するので、multi-word-component-namesを無効化
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+  skipFormatting,
+)
